986b6297adcda81be91b1fcc2982838bda632a6e
Andrea Serboli <andrea.serboli12@gmail.com>
2021-02-16 16:34:27 +0100

    stampato con index controller in tabella

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,6 +9,32 @@
     <link rel="stylesheet" href=" {{ asset('css/app.css') }} ">
 </head>
 <body>
+    <div class="container">
+        <table class="table  table-striped table-bordered">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Titolo</th>
+                    <th>Sottotitolo</th>
+                    <th>Autore</th>
+                    <th>Testo</th>
+                    <th>Anno Pubblicazione</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($articles as $article)
+                <tr>
+                    <td> {{ $article->id }} </td>
+                    <td> {{ $article->title }} </td>
+                    <td> {{ $article->subtitle }} </td>
+                    <td> {{ $article->author }} </td>
+                    <td> {{ $article->text }} l</td>
+                    <td> {{ $article->pubblication_year }}</td>
+                </tr>      
+                @endforeach
+            </tbody>
+        </table>
+    </div>
     
 </body>
 </html>
